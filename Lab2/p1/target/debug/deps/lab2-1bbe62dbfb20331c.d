6ddd99392bf8bb119a83945af66d448187179e63
LazurcaDaniel <115162904+LazurcaDaniel@users.noreply.github.com>
2023-10-18 21:07:13 +0300

    Lab2

--- Lab2/p1/target/debug/deps/lab2-1bbe62dbfb20331c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Daniel\Rust 2023\Lab2\lab2\target\debug\deps\lab2-1bbe62dbfb20331c.rmeta: src\main.rs
-
-C:\Users\Daniel\Rust 2023\Lab2\lab2\target\debug\deps\lab2-1bbe62dbfb20331c.d: src\main.rs
-
-src\main.rs:
--- Lab2/p1/target/debug/lab2.d
+++
@@ -1,1 +0,0 @@
-c:\Users\Daniel\Rust\ 2023\Lab2\lab2\target\debug\lab2.exe: c:\Users\Daniel\Rust\ 2023\Lab2\lab2\src\main.rs
